7c6e8a531fb6f187bd56490d9e7925b6bddc7a0e
krayu <51373934+krayu@users.noreply.github.com>
2020-02-25 17:30:10 +0000

    Added index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+	<meta charset="utf-8">
+	<title>BedfordPrinter.co.uk - the best online printing service in Bedford</title>
+	<meta name="description" content="bedford, printer, printing">
+	<meta name="viewport" content="width=1000">
+	<link rel="shortcut icon" href="/favicon.ico">
+	<link href="style.css" rel="stylesheet" type="text/css"/>
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans:200,400,600,700,800" rel="stylesheet" type="text/css">
+	<script type='text/javascript' src='jquery-1.11.0.min.js'></script>
+	<script type='text/javascript' src='scripts.js'></script>
+</head>
+<body>
+</body>
+</html>
