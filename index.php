a4dc94e01986b363cb5141c43e1263f3a51c3ad6
krayu <51373934+krayu@users.noreply.github.com>
2020-05-14 14:39:46 +0100

    Dislpaying value

--- index.php
+++ index.php
@@ -222,6 +222,7 @@
 					<div id="basket_text_pre" class="basket_text">
 						<?php echo $text['basket'][0]; ?>	
 					</div>
-					<div id="basket_text_post" class="basket_text">					
+					<div id="basket_text_post" class="basket_text">	
+						<?php echo $text['basket'][1]; ?>					
 </body>
 </html>
